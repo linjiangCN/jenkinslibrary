f9f08d041ede7ed3a934e61f0c866ba330f4fe85
linjiangCN <36223879+linjiangCN@users.noreply.github.com>
2020-11-21 19:28:14 +0800

    Update sonarqubeJAVA.groovy

--- src/org/devops/sonarqubeJAVA.groovy
+++ src/org/devops/sonarqubeJAVA.groovy
@@ -3,21 +3,17 @@ package org.devops
 
 //scan
 def SonarScan(projectName,projectDesc,javaPath){
+        withSonarQubeEnv("sonarqube")
         def scannerHome = "/usr/local/tcsa/base_service/sonar-scanner-4.2.0.1873-linux"
-        def sonarServer = "http://10.10.3.220:9000"
-        def sonarUser = "admin"
-        def sonarPass = "admin"
         def sonarDate = sh  returnStdout: true, script: 'date  +%Y%m%d%H%M%S'
         sonarDate = sonarDate - "\n"
     
         
         sh """ 
-              cd  ${javaPath} && ${scannerHome}/bin/sonar-scanner  -Dsonar.host.url=${sonarServer} \
+              cd  ${javaPath} && ${scannerHome}/bin/sonar-scanner  \
               -Dsonar.projectKey=${projectName} \
               -Dsonar.projectName=${projectName}  \
               -Dsonar.projectVersion=${sonarDate} \
-              -Dsonar.login=${sonarUser} \
-              -Dsonar.password=${sonarPass} \
               -Dsonar.ws.timeout=30 \
               -Dsonar.projectDescription=${projectDesc} \
               -Dsonar.links.homepage=http://www.baidu.com \
