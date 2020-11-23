38a076a64478af259d9f1c22b9f43bbef6da1224
linjiangCN <36223879+linjiangCN@users.noreply.github.com>
2020-11-23 10:45:00 +0800

    Create sonarqubeWEB.groovy

---
+++ src/org/devops/sonarqubeWEB.groovy
@@ -0,0 +1,24 @@
+package org.devops
+
+
+//scan
+def SonarScan(projectName,projectDesc){
+        withSonarQubeEnv("sonarqube-service"){
+        def scannerHome = "/usr/local/tcsa/base_service/sonar-scanner-4.2.0.1873-linux"
+        def sonarDate = sh  returnStdout: true, script: 'date  +%Y%m%d%H%M%S'
+        sonarDate = sonarDate - "\n"
+    
+        
+        sh """ 
+              cd  ${javaPath} && ${scannerHome}/bin/sonar-scanner  \
+              -Dsonar.projectKey=${projectName} \
+              -Dsonar.projectName=${projectName}  \
+              -Dsonar.projectVersion=${sonarDate} \
+              -Dsonar.ws.timeout=30 \
+              -Dsonar.projectDescription=${projectDesc} \
+              -Dsonar.links.homepage=http://www.baidu.com \
+              -Dsonar.sources=src \
+              -Dsonar.sourceEncoding=UTF-8
+        """
+    }
+}
